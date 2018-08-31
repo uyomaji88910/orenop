8685f8e3f8da3cb1760d04cae602e310e0e2ea69
ryo.nakajima <ryo.nakajima@rakuten.com>
2018-08-31 02:49:59 +0000

    mod view for UAT

--- resources/views/lists/listbar.blade.php
+++ resources/views/lists/listbar.blade.php
@@ -1,8 +1,8 @@
 
 <ul class="nav nav-tabs">
- <li role="presentation" class="{{ Request::is('lists/absent') ? 'tab-col' : 'active' }}"><a href="{{ route('lists.absent') }}">Absents <span class="badge"> {{$count['count_absent']}}</span></a></li>
- <li role="presentation" class="{{ Request::is('lists/late') ? 'tab-col' : 'active' }}"><a href="{{ route('lists.late') }}">Lates <span class="badge"> {{$count['count_late']}}</span></a></li>
- <li role="presentation" class="{{ Request::is('lists/notattend') ? 'tab-col' : 'active' }}"><a href="{{ route('lists.notattend') }}">No Status <span class="badge">{{$count['count_notattend']}}</span></a></li>
- <li role="presentation" class="{{ Request::is('lists/attend') ? 'tab-col' : 'active' }}"><a href="{{ route('lists.attend') }}"> Attends <span class="badge badge-light"> {{$count['count_attend']}}</span></a></li>
- <li role="presentation" class="{{ Request::is('lists/paid') ? 'tab-col' : 'active' }}"><a href="{{ route('lists.paid') }}"> Paid Holiday <span class="badge badge-light"> {{$count['count_paid']}}</span></a></li> 
+ <li role="presentation" class="{{ Request::is('lists/absent') ? 'tab-col' : ''}}" ><a href="{{ route('lists.absent') }}"  class="tab-class">Absents <span class="badge"> {{$count['count_absent']}}</span></a></li>
+ <li role="presentation" class="{{ Request::is('lists/late') ? 'tab-col' : '' }}"><a href="{{ route('lists.late') }}" class="tab-class">Lates <span class="badge"> {{$count['count_late']}}</span></a></li>
+ <li role="presentation" class="{{ Request::is('lists/notattend') ? 'tab-col' : '' }}"><a href="{{ route('lists.notattend') }}" class="tab-class">No Status <span class="badge">{{$count['count_notattend']}}</span></a></li>
+ <li role="presentation" class="{{ Request::is('lists/attend') ? 'tab-col' : '' }}"><a href="{{ route('lists.attend') }}" class="tab-class"> Attends <span class="badge badge-light"> {{$count['count_attend']}}</span></a></li>
+ <li role="presentation" class="{{ Request::is('lists/paid') ? 'tab-col' : '' }}"><a href="{{ route('lists.paid') }}" class="tab-class"> Paid Holiday <span class="badge badge-light"> {{$count['count_paid']}}</span></a></li> 
 </ul>
